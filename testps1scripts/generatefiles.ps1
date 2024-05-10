53b0ab15b8399b924348d075e842343d314cb36c
Alexsander-j <alexsanderjuan13@gmail.com>
2024-05-10 09:10:15 -0300

    update

--- testps1scripts/generatefiles.ps1
+++
@@ -1,7 +0,0 @@
-$path = Join-Path $pwd "test.txt"
-$size = 1GB
-$content = New-Object byte[] $size
-(New-Object System.Random).NextBytes($content)
-
-# Set-Content is very slow, use .NET method directly
-[System.IO.File]::WriteAllBytes($path, $content)
